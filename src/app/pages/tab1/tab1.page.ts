eff19b98116df71a1128af582d51518eba0c5209
bl0pez <blopez4434@gmail.com>
2023-01-25 20:20:16 -0300

    mostrando articulos

--- src/app/pages/tab1/tab1.page.ts
+++ src/app/pages/tab1/tab1.page.ts
@@ -1,4 +1,5 @@
 import { Component, OnInit } from '@angular/core';
+import { Article } from '../../interfaces';
 import { NewsService } from '../../sercises/news.service';
 
 @Component({
@@ -7,13 +8,17 @@ import { NewsService } from '../../sercises/news.service';
   styleUrls: ['tab1.page.scss']
 })
 export class Tab1Page implements OnInit {
+  
+  // Creamos un arreglo de tipo Article
+  public articles: Article[] = [];
 
   constructor( private NewsService: NewsService) {}
 
+
   ngOnInit() {
     this.NewsService.getTopHeadlines()
-      .subscribe( resp => {
-        
+      .subscribe( articles => {
+        this.articles.push(...articles);
       });
   }
 
