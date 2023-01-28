ed74bf223eea0e345b4e39539f9ac1d5f19ae372
bl0pez <blopez4434@gmail.com>
2023-01-28 09:47:13 -0300

    implementar infinite-scroll

--- src/app/pages/tab2/tab2.page.ts
+++ src/app/pages/tab2/tab2.page.ts
@@ -24,13 +24,28 @@ export class Tab2Page implements OnInit {
       })
   }
 
-  segmentChanged(category: any) {
-    this.selectedCategory = category.detail.value;
+  segmentChanged(category: Event) {
+    this.selectedCategory = (category as CustomEvent).detail.value;
 
     this.newsService.getTopHeadlinesCategory(this.selectedCategory)
       .subscribe(articles => {
         this.articles = [...articles]
       })
+  }
+
+  loadData( event: any){
+    this.newsService.getTopHeadlinesCategory(this.selectedCategory, true)
+      .subscribe(articles => {
+
+        //Comprobamos si el ultimo articulo es igual al articulo en memoria
+        if (articles[articles.length - 1].title === this.articles[this.articles.length - 1].title) {
+          event.target.disabled = true;
+          return;
+        }
+
+        this.articles = articles;
+        event.target.complete();
+      });
   }
 
 }
