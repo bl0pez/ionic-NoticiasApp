e1733d7bde773beacce3d7132161210255e67e98
bl0pez <blopez4434@gmail.com>
2023-01-28 18:52:21 -0300

    infinite scroll en para ti

--- src/app/pages/tab1/tab1.page.ts
+++ src/app/pages/tab1/tab1.page.ts
@@ -1,6 +1,7 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, OnInit, ViewChild } from '@angular/core';
 import { Article } from '../../interfaces';
 import { NewsService } from '../../sercises/news.service';
+import { IonInfiniteScroll } from '@ionic/angular';
 
 @Component({
   selector: 'app-tab1',
@@ -8,6 +9,9 @@ import { NewsService } from '../../sercises/news.service';
   styleUrls: ['tab1.page.scss']
 })
 export class Tab1Page implements OnInit {
+
+  // Seleccionamos el elemento del DOM
+  @ViewChild(IonInfiniteScroll, {static: true} ) infiniteScroll!: IonInfiniteScroll;
   
   // Creamos un arreglo de tipo Article
   public articles: Article[] = [];
@@ -19,6 +23,22 @@ export class Tab1Page implements OnInit {
     this.NewsService.getTopHeadlines()
       .subscribe( articles => {
         this.articles.push(...articles);
+      });
+  }
+
+  loadData(){
+    this.NewsService.getTopHeadlinesCategory('business', true)
+      .subscribe(articles => {
+
+        //Comprobamos si el ultimo articulo es igual al articulo en memoria
+        if (articles[articles.length - 1].title === this.articles[this.articles.length - 1].title) {
+          //Desactivamos el infinite scroll
+          this.infiniteScroll.disabled = true;
+          return;
+        }
+
+        this.articles = articles;
+        this.infiniteScroll.complete();
       });
   }
 
