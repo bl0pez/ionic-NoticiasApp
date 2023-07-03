ac1b3ce2dd89b8cf8706f39356231a75ffaa8afe
bl0pez <blopez4434@gmail.com>
2023-07-03 17:59:04 -0400

    Cargar las noticias del storage

--- src/app/pages/tab3/tab3.module.ts
+++ src/app/pages/tab3/tab3.module.ts
@@ -3,7 +3,10 @@ import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
 import { FormsModule } from '@angular/forms';
 import { Tab3Page } from './tab3.page';
+
+//Modulos
 import { ExploreContainerComponentModule } from '../../explore-container/explore-container.module';
+import { ComponentsModule } from 'src/app/components/components.module';
 
 import { Tab3PageRoutingModule } from './tab3-routing.module';
 
@@ -13,7 +16,8 @@ import { Tab3PageRoutingModule } from './tab3-routing.module';
     CommonModule,
     FormsModule,
     ExploreContainerComponentModule,
-    Tab3PageRoutingModule
+    Tab3PageRoutingModule,
+    ComponentsModule,
   ],
   declarations: [Tab3Page]
 })
--- src/app/services/storage.service.ts
+++ src/app/services/storage.service.ts
@@ -11,13 +11,21 @@ export class StorageService {
   private _storage: Storage | null = null;
   private _localArticles: Article[] = [];
 
-  constructor(private storage: Storage) { 
+  constructor(private storage: Storage) {
     this.init();
+  }
+
+  //retornamos una copia del array de articulos
+  get getLocalArticles() {
+    return [...this._localArticles];
   }
 
   async init() {
     const storage = await this.storage.create();
     this._storage = storage;
+
+    this.loadFavorites();
+
   }
 
   async saveRemoveArticle(article: Article) {
@@ -26,14 +34,28 @@ export class StorageService {
     const exist = this._localArticles.find(art => art.title === article.title);
 
     //Si existe devuelvo el array sin el articulo
-    if(exist){
+    if (exist) {
       this._localArticles = this._localArticles.filter(localArticle => localArticle.title !== article.title);
-    }else{
+    } else {
       this._localArticles = [article, ...this._localArticles]
     }
 
 
     this._storage?.set('articles', this._localArticles);
+  }
+
+  async loadFavorites() {
+    try {
+
+      //Obtenemos los articulos del localstore
+      const articles = await this._storage?.get('articles');
+      //Establecemos los articulos 
+      this._localArticles = articles || [];
+
+    } catch (error) {
+
+
+    }
   }
 
 }
